263d8cc6ac1f742d398f360360ce8213571347ae
farhanacsebd <farhana.cse.bd@gmail.com>
2023-02-09 22:51:08 +0600

    added cursor love

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const bodyE1 = document.querySelector("body");
+
+bodyE1.addEventListener("mousemove",(e) => {
+    const xPos = e.offsetX; 
+    const yPos = e.offsetY; 
+    const spanE1 = document.createElement("span");
+    spanE1.style.left = xPos + "px"; 
+    spanE1.style.top = yPos + "px"; 
+
+    const size = Math.random() * 100;
+    spanE1.style.width = size + "px";
+    spanE1.style.height = size + "px";
+
+    bodyE1.appendChild(spanE1);
+
+    setTimeout(() => {
+        spanE1.remove();
+    },3000);
+
+})
